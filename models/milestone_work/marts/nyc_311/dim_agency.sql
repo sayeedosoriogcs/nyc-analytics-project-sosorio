3eb2bfff844a05a457bd61d8f3983a0140874c6a
Sayeed Osorio <sayeedosorio.gcs@gmail.com>
2026-05-04 19:13:55 +0000

    dim_agency created

---
+++ models/milestone_work/marts/nyc_311/dim_agency.sql
@@ -0,0 +1,37 @@
+
+with source as (
+
+    select *
+    from {{ ref('stg_nyc_311_sr') }}
+
+),
+
+filtered as (
+
+    select *
+    from source
+    where lower(trim(complaint_type)) = 'illegal parking'
+
+),
+
+agencies as (
+
+    select distinct
+        agency       as agency_abbreviation,
+        agency_name  as agency_name
+    from filtered
+    where agency is not null
+
+),
+
+final as (
+
+    select
+        {{ dbt_utils.generate_surrogate_key(['agency_abbreviation']) }} as agency_key,
+        agency_name,
+        agency_abbreviation
+    from agencies
+
+)
+
+select * from final
