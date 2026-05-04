638344f65edec4b4c09cdb828520e3cddaa2b68f
Sayeed Osorio <sayeedosorio.gcs@gmail.com>
2026-05-04 01:33:07 +0000

    change test 311

--- models/assignment_work/staging/test_311_sample.sql
+++ models/assignment_work/staging/test_311_sample.sql
@@ -4,5 +4,5 @@
      created_date,
      complaint_type,
      borough
- FROM {{ source('raw', 'source_dot_service_requests_history') }}
+ FROM {{ source('raw', 'nyc_311_service_requests') }}
  LIMIT 10
