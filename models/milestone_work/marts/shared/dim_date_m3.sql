3ceb3cd170eb10bd20a4f7973336ff6bf80b9bd0
Sayeed Osorio <sayeedosorio.gcs@gmail.com>
2026-05-04 18:45:42 +0000

    change date

--- models/milestone_work/marts/shared/dim_date_m3.sql
+++ models/milestone_work/marts/shared/dim_date_m3.sql
@@ -3,7 +3,7 @@ with date_spine as (
     {{
         dbt_utils.date_spine(
             datepart="day",
-            start_date="cast('2015-01-01' as date)",
+            start_date="cast('2022-01-01' as date)",
             end_date="cast('2026-12-31' as date)"
         )
     }}
