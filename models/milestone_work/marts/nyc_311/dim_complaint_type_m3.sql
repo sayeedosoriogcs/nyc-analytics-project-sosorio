7df7ccf358b7a67599e798ffb730eb3ccd797255
Sayeed Osorio <sayeedosorio.gcs@gmail.com>
2026-05-04 18:49:58 +0000

    complaint type

--- models/milestone_work/marts/nyc_311/dim_complaint_type_m3.sql
+++ models/milestone_work/marts/nyc_311/dim_complaint_type_m3.sql
@@ -1,12 +1,29 @@
-WITH source AS (
-    SELECT * 
-    FROM {{ ref('stg_nyc_311_sr') }}
+with source as (
+
+    select *
+    from {{ ref('stg_nyc_311_sr') }}
+
 ),
 
-filtered AS (
-    SELECT *
-    FROM source
-    WHERE LOWER(TRIM(complaint_type)) = 'illegal parking'
+complaint_types as (
+
+    select distinct
+        complaint_type,
+        descriptor as complaint_category
+    from source
+    where lower(trim(complaint_type)) = 'illegal parking'
+      and complaint_type is not null
+
+),
+
+final as (
+
+    select
+        {{ dbt_utils.generate_surrogate_key(['complaint_type']) }} as complaint_type_key,
+        complaint_type                                              as complaint_type_name,
+        complaint_category
+    from complaint_types
+
 )
 
-SELECT * FROM filtered
+select * from final
